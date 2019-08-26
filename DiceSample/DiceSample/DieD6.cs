900b122a33e0041eebc0fb19c9e4a874590557e7
mkroth93 <46752738+mkroth93@users.noreply.github.com>
2019-08-26 10:50:51 -0500

    Added Dice Project

---
+++ DiceSample/DiceSample/DieD6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace DiceSample
+{
+    class DieD6 : PictureBox
+
+    {
+        private int score;
+        private int faceValue;
+
+        public int Score { get => score; set => score = value; }
+        public int FaceValue { get => faceValue; set => faceValue = value; }
+    }
+}
---
+++ DiceSample/DiceSample/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DieD6 pictureBox2 = new DieD6();
+
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Score = 7;
+            pictureBox2.Image = global::DiceSample.Properties.Resources.dice_six_faces_two;
+            pictureBox2.Location = new System.Drawing.Point(133, 90);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(260, 77);
+            pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+
+            this.Controls.Add(pictureBox2);
+        }
+    }
+}
